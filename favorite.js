070123cda4225567d5df6be8bb45ed66475649d9
maayan <maayanramot1@gmail.com>
2023-01-20 15:54:05 +0200

    last commit

---
+++ favorite.js
@@ -0,0 +1,53 @@
+"use strict";
+
+import data from "./pokedex.json" assert { type: "json" };
+
+//get from storage
+const entries = Object.entries(localStorage);
+console.log(entries);
+console.log(entries[1][1]);
+for (let i = 0; i < entries.length; i++) {
+    let idElement = entries[i][1];
+    console.log(idElement);
+}
+
+
+// grid
+function grid() {
+    
+    for (let i = 0; i < entries.length; i++) {
+    let idElement = entries[i][1] - 1;
+    //card
+    const card = document.createElement("button");
+    card.setAttribute("id", `${data[idElement]["id"]}`);
+    card.className = "card";
+
+    //id
+    const id = document.createElement("p");
+    id.innerText = `#00${data[idElement]["id"]}`;
+    id.className = "id";
+    card.appendChild(id);
+
+    //image 
+    const image = document.createElement("img");
+    image.src = `${data[idElement]["image"]["hires"]}`;
+    image.className = "image";
+    card.appendChild(image);
+
+    //name
+    const name = document.createElement("p");
+    name.innerText = `${data[idElement]["name"]["english"]}`;
+    name.className = "name";
+    card.appendChild(name);
+
+    document.getElementsByClassName("grid")[0].appendChild(card);
+  }
+}
+grid();
+
+const homePage = document.getElementById("homePage");
+homePage.addEventListener("click", openHomePage);
+//open home page
+function openHomePage() {
+  window.open("http://127.0.0.1:5500/index.html", "_self");
+}
--- main.js
+++ main.js
@@ -2,103 +2,120 @@
 
 import data from "./pokedex.json" assert { type: "json" };
 
-function createPokeGrid() {
+// grid
+function grid() {
   for (let i = 0; i < 12; i++) {
-    const pokeCard = document.createElement("button");
-    pokeCard.className = "pokemon button";
+    //card
+    const card = document.createElement("button");
+    card.setAttribute("id", `${data[i]["id"]}`);
+    card.className = "card";
 
-    const pokeID = document.createElement("p");
-    pokeID.innerText = `#00${data[i]["id"]}`;
-    pokeCard.appendChild(pokeID);
-    pokeID.className = "poke-id";
+    //id
+    const id = document.createElement("p");
+    id.innerText = `#00${data[i]["id"]}`;
+    id.className = "id";
+    card.appendChild(id);
 
-    pokeCard.setAttribute("id", `${data[i]["id"]}`);
+    //image 
+    const image = document.createElement("img");
+    image.src = `${data[i]["image"]["hires"]}`;
+    image.className = "image";
+    card.appendChild(image);
 
-    const pokeImg = document.createElement("img");
-    pokeImg.src = `${data[i]["image"]["hires"]}`;
-    pokeCard.appendChild(pokeImg);
-    pokeImg.className = "poke-img";
+    //name
+    const name = document.createElement("p");
+    name.innerText = `${data[i]["name"]["english"]}`;
+    name.className = "name";
+    card.appendChild(name);
 
-    const pokeName = document.createElement("p");
-    pokeName.innerText = `${data[i]["name"]["english"]}`;
-    pokeCard.appendChild(pokeName);
-    pokeName.className = "poke-name";
-
-    const pokeGrid = document.getElementsByClassName("poke-grid");
-    pokeGrid[0].appendChild(pokeCard);
+    document.getElementsByClassName("grid")[0].appendChild(card);
   }
 }
-createPokeGrid();
+grid();
 
+//----------------------------------------------------> modal
 function createModal(idElem) {
-  const pokeModalContent = document.getElementsByClassName("modal-content");
+  const modalContent = document.getElementsByClassName("modal-content");
 
-  const modalLeftPart = document.createElement("div");
-  modalLeftPart.className = "modal-left-part";
+  //left
+  const left = document.createElement("div");
+  left.className = "modal-left-part";
 
-  const modalPokeID = document.createElement("p");
-  modalPokeID.innerText = `#00${data[idElem]["id"]}`;
-  modalLeftPart.appendChild(modalPokeID);
-  modalPokeID.className = "modal-poke-id";
+  //modal id
+  const modalID = document.createElement("p");
+  modalID.innerText = `#00${data[idElem]["id"]}`;
+  modalID.className = "modal-poke-id";
+  left.appendChild(modalID);
 
-  const modalPokeImg = document.createElement("img");
-  modalPokeImg.src = `${data[idElem]["image"]["hires"]}`;
-  modalLeftPart.appendChild(modalPokeImg);
-  modalPokeImg.className = "modal-poke-img";
+  //modal image
+  const modalImg = document.createElement("img");
+  modalImg.src = `${data[idElem]["image"]["hires"]}`;
+  modalImg.className = "modal-poke-img";
+  left.appendChild(modalImg);
 
-  const modalPokeName = document.createElement("p");
-  modalPokeName.innerText = `${data[idElem]["name"]["english"]}`;
-  modalLeftPart.appendChild(modalPokeName);
-  modalPokeName.className = "modal-poke-name";
+  //name
+  const modalName = document.createElement("p");
+  modalName.innerText = `${data[idElem]["name"]["english"]}`;
+  modalName.className = "modal-poke-name";
+  left.appendChild(modalName);
 
-  const pokeTypeCon = document.createElement("div");
-  pokeTypeCon.className = "poke-types-container";
+  //type
+  const typeContainer = document.createElement("div");
+  typeContainer.className = "poke-types-container";
+
   for (let i = 0; i < `${data[idElem]["type"].length}`; i++) {
-    const pokeType = document.createElement("button");
-    pokeType.className = "poke-type";
-    pokeType.innerText = `${data[idElem]["type"][i]}`;
-
-    pokeType.setAttribute("id", `${data[idElem]["type"][i]}`);
-
-    pokeTypeCon.appendChild(pokeType);
+    
+    const type = document.createElement("button");
+    type.innerText = `${data[idElem]["type"][i]}`;
+    type.setAttribute("id", `${data[idElem]["type"][i]}`);
+    type.className = "poke-type";
+    typeContainer.appendChild(type);
   }
-  modalLeftPart.appendChild(pokeTypeCon);
+  left.appendChild(typeContainer);
+  modalContent[0].appendChild(left);
 
-  pokeModalContent[0].appendChild(modalLeftPart);
-
-  const modalRightPart = document.createElement("div");
-  modalRightPart.className = "modal-right-part";
-
+  //line
   const line = document.createElement("div");
-  pokeModalContent[0].appendChild(line);
   line.className = "vl";
+  modalContent[0].appendChild(line);
+  
+  //right
+  const right = document.createElement("div");
+  right.className = "modal-right-part";
 
-  const descriptionDiv = document.createElement("div");
-  descriptionDiv.className = "description-div";
+  //description container
+  const descriptionContainer = document.createElement("div");
+  descriptionContainer.className = "description-container";
 
+  //title
   const descriptionTitle = document.createElement("div");
   descriptionTitle.innerText = `Description`;
-  descriptionDiv.appendChild(descriptionTitle);
   descriptionTitle.className = "description-title";
+  descriptionContainer.appendChild(descriptionTitle);
 
+  //description
   const description = document.createElement("div");
   description.innerText = `${data[idElem]["description"]}`;
-  descriptionDiv.appendChild(description);
   description.className = "description";
+  descriptionContainer.appendChild(description);
 
-  modalRightPart.appendChild(descriptionDiv);
+  right.appendChild(descriptionContainer);
 
-  const statsDiv = document.createElement("div");
-  statsDiv.className = "stats-div";
+  //stats
+  const statsContainer = document.createElement("div");
+  statsContainer.className = "stats-div";
 
+  //title
   const statsTitle = document.createElement("div");
   statsTitle.innerText = `Stats`;
-  statsDiv.appendChild(statsTitle);
   statsTitle.className = "stats-title";
+  statsContainer.appendChild(statsTitle);
 
+  //content
   const statsContent = document.createElement("div");
   statsContent.className = "stats-content";
 
+  //left
   const statsContentLeft = document.createElement("div");
   statsContentLeft.className = "stats-content-left";
 
@@ -121,6 +138,7 @@ function createModal(idElem) {
 
   statsContent.appendChild(statsContentLeft);
 
+  //right
   const statsContentRight = document.createElement("div");
   statsContentRight.className = "stats-content-right";
 
@@ -143,54 +161,79 @@ function createModal(idElem) {
   statsContentRight.appendChild(pokeStats6);
 
   statsContent.appendChild(statsContentRight);
+  
+  //total stats calculate
+  const statsArray = [
+    `${data[idElem]["base"]["HP"]}`,
+    `${data[idElem]["base"]["Attack"]}`,
+    `${data[idElem]["base"]["Defense"]}`,
+    `${data[idElem]["base"]["Sp. Attack"]}`,
+    `${data[idElem]["base"]["Sp. Defense"]}`,
+    `${data[idElem]["base"]["Speed"]}`
+  ];
+  let sum = 0;
+  statsArray.forEach((num) => sum += parseInt(num));
 
-  statsDiv.appendChild(statsContent);
-  modalRightPart.appendChild(statsDiv);
+  const totalContainer = document.createElement("div");
+  const total = document.createElement("p");
+  total.innerText = `Total: ` + sum;
+  total.className = "stats";
+  totalContainer.appendChild(total);
+  statsContent.appendChild(totalContainer);
 
-  pokeModalContent[0].appendChild(modalRightPart);
+  statsContainer.appendChild(statsContent);
+  right.appendChild(statsContainer);
 
-  const favoriteButton = document.createElement("button");
-  favoriteButton.className = "favorite-button";
-  const favoriteButtonImg = document.createElement("img");
-  favoriteButtonImg.className = "heart-button";
-  favoriteButtonImg.src = "./assets/favorite-heart.svg";
+  modalContent[0].appendChild(right);
 
-  favoriteButton.appendChild(favoriteButtonImg);
-  pokeModalContent[0].appendChild(favoriteButton);
+  //heart button
+  const favoriteButton = document.createElement("img");
+  favoriteButton.src = "./assets/heart.png";
+  favoriteButton.className = "heart-button";
+  modalContent[0].appendChild(favoriteButton); 
+
+  let indicate = 'empty';
+  favoriteButton.addEventListener("click", changeHeart);
+  // change heart button
+  function changeHeart() {
+    if (indicate == 'empty') { 
+      favoriteButton.src = "./assets/fill-heart.png";
+      indicate = 'fill';
+      localStorage.setItem(`${data[idElem]["name"]["english"]}` ,`${data[idElem]["id"]}`)
+    } else if (indicate == 'fill') {
+      favoriteButton.src = "./assets/heart.png";
+      indicate = 'empty';
+      localStorage.removeItem(`${data[idElem]["name"]["english"]}`)
+    };
+  }
 }
 
 //Get modal elem
 let modal = document.getElementById("simpleModal");
 //Get open modal button
-let modalBtn = document.querySelectorAll(".pokemon");
-//Get close button
-let closeBtn = document.getElementsByClassName("closeBtn")[0];
-
+let modalBtn = document.querySelectorAll(".card");
 //Listen for click
 for (let i = 0; i < 12; i++) {
   modalBtn[i].addEventListener("click", openModal);
 }
-//Listen for close click
-closeBtn.addEventListener("click", closeModal);
-//Listen for outside click
-window.addEventListener("click", clickOutside);
-
 //Function to open modal
 function openModal() {
   modal.style.display = "block";
   createModal(this.id - 1);
 }
 
-function clearModalContent() {
-  let modalContent = document.getElementsByClassName("modal-content")[0];
-  modalContent.innerText = "";
-}
-
+//Get close button
+let closeBtn = document.getElementsByClassName("closeBtn")[0];
+//Listen for close click
+closeBtn.addEventListener("click", closeModal);
 //Function to close modal
 function closeModal() {
   modal.style.display = "none";
   clearModalContent();
 }
+
+//Listen for outside click
+window.addEventListener("click", clickOutside);
 //Function to close modal if outside click
 function clickOutside(e) {
   if (e.target == modal) {
@@ -198,3 +241,37 @@ function clickOutside(e) {
     clearModalContent();
   }
 }
+
+//Function to clear modal
+function clearModalContent() {
+  let modalContent = document.getElementsByClassName("modal-content")[0];
+  modalContent.innerText = "";
+}
+
+const homePage = document.getElementById("homePage");
+homePage.addEventListener("click", openHomePage);
+//open home page
+function openHomePage() {
+  window.open("http://127.0.0.1:5500/index.html", "_self");
+}
+
+const favoriteBtn = document.getElementById("favorite");
+favoriteBtn.addEventListener("click", favoritePage);
+//open favorite page
+function favoritePage() {
+  window.open("http://127.0.0.1:5500/favorite.html", "_self");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
